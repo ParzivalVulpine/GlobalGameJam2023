4f07cff316628f7224b8ffc76573d2feb87d95a7
Daniel Gaitan <mrdaniomi36@gmail.com>
2023-02-04 22:55:33 -0600

    Second Push (Movement)

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -15,11 +15,12 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     public Rigidbody2D rb;
-    float jump = 10;
-    float moveSpeed = 10;
-    int jumpCounter = 0;
-    int numOfJumps = 2;
+    public float jump = 10;
+    public float moveSpeed = 0;
+    public int jumpCounter = 0;
+    public int numOfJumps = 2;
     public int acornCounter = 0;
+<<<<<<< Updated upstream
     public bool canMove = false;
     public Camera camera;
     Vector3 start;
@@ -28,6 +29,15 @@ public class PlayerScript : MonoBehaviour
     {
         start = new Vector3(gameObject.transform.position.x,gameObject.transform.position.y,gameObject.transform.position.z);
     }
+=======
+    public int accceleration = 5;
+    public int maxSpeed = 30;
+    public bool canMove = false;
+    public Camera camera;
+    Vector2 movementControl;
+    private bool isFacingRight = true;
+    private float horizontal;
+>>>>>>> Stashed changes
 
     // Update is called once per frame
     void Update()
@@ -35,54 +45,53 @@ public class PlayerScript : MonoBehaviour
         if (canMove)
         {
             //Checks key presses for movement
-            if (Input.GetKey("d") == true)
-            {
-                MoveRight();
-            }
-            if (Input.GetKey("a") == true)
-            {
-                MoveLeft();
-            }
-            if (Input.GetKeyDown("space") == true)
+            horizontal = Input.GetAxisRaw("Horizontal");
+
+            if (Input.GetButtonDown("Jump"))
             {
                 Jump();
             }
+            Flip();
         }
+
         if (camera.transform.position.y <= gameObject.transform.position.y)
         {
             canMove = true;
         }
+<<<<<<< Updated upstream
         
         
         
+=======
+
+
+>>>>>>> Stashed changes
     }
-    //Moves the player right
-    void MoveRight()
+
+    private void FixedUpdate()
     {
-        transform.position = transform.position + new Vector3(moveSpeed, 0, 0) * Time.deltaTime * Time.timeScale;
+        rb.velocity = new Vector2(horizontal * maxSpeed, rb.velocity.y);
     }
-    //Moves player left
-    void MoveLeft() {
-        transform.position = transform.position + new Vector3(-moveSpeed, 0, 0) * Time.deltaTime * Time.timeScale; 
-    }
+
     //Jumps
     void Jump()
     {
         // if checkJump returns true, jump
-        if(checkJump())
+        if (checkJump())
         {
             if (jumpCounter < numOfJumps)
             {
-                rb.velocity = new Vector2(rb.velocity.x,jump);
+                rb.velocity = new Vector2(rb.velocity.x, jump);
                 jumpCounter = jumpCounter + 1;
             }
         }
-        
-        
+
     }
+
     //Resets the jumpCounter to zero upon colliding with an object
     private void OnCollisionEnter2D(Collision2D collision)
     {
+<<<<<<< Updated upstream
         if(collision.gameObject.tag == "Ground" && gameObject.transform.position.y > collision.gameObject.transform.position.y)
         {
             jumpCounter = 0;
@@ -100,7 +109,14 @@ public class PlayerScript : MonoBehaviour
        
          
         
+=======
+        if (collision.gameObject.tag == "Ground")
+        {
+            jumpCounter = 0;
+        }
+>>>>>>> Stashed changes
     }
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Acorn")
@@ -108,12 +124,20 @@ public class PlayerScript : MonoBehaviour
             acornCounter++;
             Destroy(collision.gameObject);
         }
+<<<<<<< Updated upstream
         
         
         
         
+=======
+
+
+
+        Destroy(collision.gameObject);
+>>>>>>> Stashed changes
     }
     //Checks if player can jump
+
     private bool checkJump()
     {
         // If player has not jumped the number of jumps allowed, return true
@@ -127,8 +151,20 @@ public class PlayerScript : MonoBehaviour
         }
     }
     //Gets the value of the acornCounter variable
+    
     public int GetAcornCounter()
     {
         return acornCounter;
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
     }
 }
